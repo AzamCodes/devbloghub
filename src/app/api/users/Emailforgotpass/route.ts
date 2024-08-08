042a2bfb0b1952dc3e0c1c454535722c4f6f692b
AzamCodes <azam.shaikh9105@gmail.com>
2024-08-08 23:43:45 +0530

    email forgot

--- src/app/api/users/Emailforgotpass/route.ts
+++ src/app/api/users/Emailforgotpass/route.ts
@@ -3,58 +3,40 @@ import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "@/models/UserModel";
+import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
 export async function POST(request: NextRequest) {
   try {
     const reqBody = await request.json();
-    const { email, password } = reqBody;
+    console.log("This is reqbody", reqBody);
+
+    const { email } = reqBody.email;
+    console.log(email); // Extract email from request body
+    if (!email) {
+      return NextResponse.json({ error: "Email is required" }, { status: 400 });
+    }
 
     const user = await User.findOne({ email });
+    console.log("Found user:", user);
 
     if (!user) {
-      return NextResponse.json({
-        error: "Invalid Credentials",
-        status: 400,
-      });
+      return NextResponse.json({ error: "User Not Found" }, { status: 400 });
     }
 
-    const validPassword = await bcryptjs.compare(password, user.password);
-    if (!validPassword) {
-      return NextResponse.json({
-        error: "Invalid Credentials",
-        status: 400,
-      });
-    }
+    sendEmail({ email, emailType: "RESET", userId: user._id });
 
-    const tokenData = {
-      id: user._id,
-      email: user.email,
-      username: user.username,
-    };
-
-    const token = await jwt.sign(tokenData, process.env.SECRET_TOKEN!, {
-      expiresIn: "1d",
-    });
-
-    const response = NextResponse.json({
-      message: "Logged in successfully!",
-      success: 200,
-      token, // Include token in the response body
-    });
-
-    response.cookies.set("token", token, {
-      httpOnly: true, // Keeps the cookie secure
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      path: "/",
-    });
-
-    return response;
-  } catch (error: any) {
     return NextResponse.json({
-      error: error.message,
+      message: "Email for Forgot Password Sent successfully",
+      success: true,
     });
+  } catch (error: any) {
+    console.error("Error sending reset email:", error);
+    // Consider a more descriptive error message for the user based on error type
+    return NextResponse.json(
+      { message: "An error occurred." },
+      { status: 500 }
+    );
   }
 }
